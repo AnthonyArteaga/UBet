e7874d90e2c1908eb9db9c4a5717a4a028b90763
Anthony Artega <anthony.m.arteaga@gmail.com>
2024-11-09 15:25:12 -0500

    fixed issue with db

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-app.config["MONGO_URI"] = "mongodb+srv://arteaga215:admin@cluster.mu9ti.mongodb.net/"
+app.config["MONGO_URI"] = "mongodb+srv://arteaga215:admin@cluster.mu9ti.mongodb.net/db"
 mongo = PyMongo(app)
 
 @app.route('/')
@@ -20,16 +20,16 @@ def add_message():
     data = request.json
     message = data.get("message")
     if message:
-        mongo.db.messages.insert_one({
+        mongo.db.users.insert_one({
             "message": message,
-            "timestamp": datetime.utcnow()
+            "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         })
         return jsonify(status="Message saved!", message=message), 201
     return jsonify(error="No message provided"), 400
 
 @app.route('/messages', methods=['GET'])
 def get_messages():
-    messages = list(mongo.db.messages.find({}, {"_id": 0, "message": 1, "timestamp": 1}))
+    messages = list(mongo.db.users.find({}, {"_id": 0, "message": 1, "timestamp": 1}))
     return jsonify(messages=messages)
 
 if __name__ == "__main__":
